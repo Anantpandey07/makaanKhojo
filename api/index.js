60487aa5e603540e000bed6b7e5af1c049bcb0ec
Anantpandey07 <anantpandey175@gmail.com>
2024-06-26 15:42:57 +0530

    create a middleware and test api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user-route.js";
+import authRoute from "./routes/auth-route.js";
 dotenv.config();
 
 mongoose
@@ -13,9 +14,22 @@ mongoose
     console.log("error!!");
 });
 const app = express();
-
+app.use(express.json());
 app.listen(3000, ()=>{
     console.log('server is running at port 3000');
 });
 
 app.use('/api/user', userRoute);
+
+app.use('/api/auth', authRoute);
+
+
+app.use((err, req, res, next) =>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
